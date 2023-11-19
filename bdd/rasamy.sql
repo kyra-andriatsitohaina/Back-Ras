5b505e0627c6677934e0f3cc27c55901507193e9
user1 <user1@atmail.com>
2023-11-19 21:38:10 +0300

    bdd ~ k1

--- bdd/rasamy.sql
+++ bdd/rasamy.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : dim. 19 nov. 2023 à 08:56
+-- Généré le : dim. 19 nov. 2023 à 18:23
 -- Version du serveur : 10.6.5-MariaDB
 -- Version de PHP : 7.4.26
 
@@ -38,7 +38,7 @@ CREATE TABLE IF NOT EXISTS `articles` (
   `amenties` varchar(255) NOT NULL,
   `category` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Déchargement des données de la table `articles`
@@ -54,7 +54,9 @@ INSERT INTO `articles` (`id`, `title`, `description`, `price`, `image`, `type`, 
 (7, ' kely  ', 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. In praesentium incidunt architecto veniam voluptate quisquam voluptates ut quasi nisi repellat eligendi magnam, nesciunt illo atque id dolores? Corrupti fuga veritatis, assumenda eaque quos praesentium mollitia necessitatibus dolor laborum ratione numquam ea nostrum maxime ipsa nesciunt. Ut, quae commodi vero cumque dolorum sint sunt natus omnis blanditiis perspiciatis deserunt! Eligendi ad voluptatibus voluptates voluptate laudantium praesentium corrupti similique assumenda asperiores. Nihil, veritatis? Minima perferendis, dignissimos quibusdam dolorem accusamus veniam nisi ab?', 25, 'sary2.jpg', 'rent', 'parking', 'category 1'),
 (8, ' villa ', 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. In praesentium incidunt architecto veniam voluptate quisquam voluptates ut quasi nisi repellat eligendi magnam, nesciunt illo atque id dolores? Corrupti fuga veritatis, assumenda eaque quos praesentium mollitia necessitatibus dolor laborum ratione numquam ea nostrum maxime ipsa nesciunt. Ut, quae commodi vero cumque dolorum sint sunt natus omnis blanditiis perspiciatis deserunt! Eligendi ad voluptatibus voluptates voluptate laudantium praesentium corrupti similique assumenda asperiores. Nihil, veritatis? Minima perferendis, dignissimos quibusdam dolorem accusamus veniam nisi ab?', 250, 'sary3.jpg', 'sale', 'parking', 'category 2'),
 (9, ' villa be ', 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. In praesentium incidunt architecto veniam voluptate quisquam voluptates ut quasi nisi repellat eligendi magnam, nesciunt illo atque id dolores? Corrupti fuga veritatis, assumenda eaque quos praesentium mollitia necessitatibus dolor laborum ratione numquam ea nostrum maxime ipsa nesciunt. Ut, quae commodi vero cumque dolorum sint sunt natus omnis blanditiis perspiciatis deserunt! Eligendi ad voluptatibus voluptates voluptate laudantium praesentium corrupti similique assumenda asperiores. Nihil, veritatis? Minima perferendis, dignissimos quibusdam dolorem accusamus veniam nisi ab?', 340, 'sary4.jpg', 'rent', 'furnished', 'category 3'),
-(10, ' belou kely ', 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. In praesentium incidunt architecto veniam voluptate quisquam voluptates ut quasi nisi repellat eligendi magnam, nesciunt illo atque id dolores? Corrupti fuga veritatis, assumenda eaque quos praesentium mollitia necessitatibus dolor laborum ratione numquam ea nostrum maxime ipsa nesciunt. Ut, quae commodi vero cumque dolorum sint sunt natus omnis blanditiis perspiciatis deserunt! Eligendi ad voluptatibus voluptates voluptate laudantium praesentium corrupti similique assumenda asperiores. Nihil, veritatis? Minima perferendis, dignissimos quibusdam dolorem accusamus veniam nisi ab?', 34, 'sary3.jpg', 'offer', 'furnished', 'category 1');
+(10, ' belou kely ', 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. In praesentium incidunt architecto veniam voluptate quisquam voluptates ut quasi nisi repellat eligendi magnam, nesciunt illo atque id dolores? Corrupti fuga veritatis, assumenda eaque quos praesentium mollitia necessitatibus dolor laborum ratione numquam ea nostrum maxime ipsa nesciunt. Ut, quae commodi vero cumque dolorum sint sunt natus omnis blanditiis perspiciatis deserunt! Eligendi ad voluptatibus voluptates voluptate laudantium praesentium corrupti similique assumenda asperiores. Nihil, veritatis? Minima perferendis, dignissimos quibusdam dolorem accusamus veniam nisi ab?', 34, 'sary3.jpg', 'offer', 'furnished', 'category 1'),
+(11, 'testeo', 'blablabla', 50, 'sary1.jpg', 'rent', 'parking', 'category 2'),
+(12, 'sefo be', 'hdghgsdhgfkhsdkfh', 250, 'sary3.jpg', 'sale', 'furnished', 'category 2');
 
 -- --------------------------------------------------------
 
